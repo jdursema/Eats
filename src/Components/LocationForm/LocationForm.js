6fe62db6c2f68290ce14a377e13bf2b6f626f5a1
Julie Dursema <jmdursema@gmail.com>
2017-12-20 22:03:49 -0700

    set-up firebase

--- src/Components/LocationForm/LocationForm.js
+++ src/Components/LocationForm/LocationForm.js
@@ -7,15 +7,20 @@ class LocationForm extends Component {
       location: '',
     }
   }
+
+  handleLocationChange = event => {
+    this.setState({location : event.target.value})
+  }
+
+
   render() {
     return (
       <div>
         <h3>Where are you looking to eat?</h3>
         <form>
-          <input type='text'/>
+          <input type='text' onChange={this.handleLocationChange} value={this.state.location}/>
           <input type='submit'/>
         </form>
-        
       </div>
     )
   }
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  constructor(){
+    super()
+    this.state ={
+      username: '',
+      password: ''
+    }
+  }
+
+  handleStateChange = event => {
+    this.setState({[event.target.name] : event.target.value})
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const userRef = firebase.database().ref('user');
+    const user = {
+      favorites: [],
+      userName: this.state.userName,
+    }
+  }
+
+
   render() {
     return (
       <form>
         <label for='username-input'>Username: </label>
-        <input type='text' className='username-input'/>
+        <input type='text' className='username-input' onChange={this.handleStateChange} name='username'/>
         <label for='password-input'>Password: </label>
-        <input type='password' className='password-input'/>
+        <input type='password' className='password-input' onChange={this.handleStateChange} name='password' value={this.state.password}/>
+        <button>Create Users</button>
       </form>
     )
   }
