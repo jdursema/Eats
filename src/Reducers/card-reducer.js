cd282b6a7ff88b4e67c2351789cc8d170fee666b
Julie Dursema <jmdursema@gmail.com>
2018-01-07 06:43:03 -0700

    lint card-reducer

--- src/Reducers/card-reducer.js
+++ src/Reducers/card-reducer.js
@@ -1,10 +1,10 @@
 const cardReducer = (state=[], action) => {
   switch (action.type) {
-    case 'CREATE_CARDS':
-      return [...action.restaurantArray];
-    default:
-      return state;
+  case 'CREATE_CARDS':
+    return [...action.restaurantArray];
+  default:
+    return state;
   }
-}
+};
 
 export default cardReducer;
