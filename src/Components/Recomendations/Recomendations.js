865d297eeeea32a51500b68fa9626a3dabc48b2b
Julie Dursema <jmdursema@gmail.com>
2018-01-07 14:11:49 -0700

    display recommendations

--- src/Components/Recomendations/Recomendations.js
+++ src/Components/Recomendations/Recomendations.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { fetchRecommendations } from '../../Actions';
 import { connect } from 'react-redux';
+import Card from '../Card/Card';
+import { postAddFavorite, postDeleteFavorite } from '../../Actions';
 
 class Recommendations extends Component {
 
@@ -8,9 +10,31 @@ class Recommendations extends Component {
     this.props.handleRecommendations(this.props.favorites, this.props.location, this.props.cuisine)
   }
 
+  mappedCards (){
+    const cards = this.props.recommendations.map((card)=> {
+      const favRestaurant = this.props.favorites.find(favRestaurant => 
+        favRestaurant.name === card.name);
+  
+      const favorited = favRestaurant ? true : false;
+
+      return <Card 
+        info= {card}
+        handleAddFav = {this.props.handleAddFav}
+        handleDeleteFav = {this.props.handleDeleteFav}
+        user = {this.props.user}
+        history = {this.props.history}
+        favorited = {favorited}
+        favorites = {this.props.favorites}/>;
+    });
+
+    return cards;
+  }
+
   render() {
     return (
-      <h1>RECOMMENDATIONS</h1>
+      <div>
+        {this.mappedCards()}
+      </div>
     );
   }
   
@@ -19,12 +43,18 @@ class Recommendations extends Component {
 const mapStateToProps = state => ({
   favorites: state.favorites,
   location: state.location,
-  cuisine: state.cuisine
+  cuisine: state.cuisine,
+  recommendations: state.recommendations,
+  user: state.user
 });
 
 const mapDipatchToProps = dispatch => ({
   handleRecommendations: (favsArray, locationObj, cuisineArray) =>
-    dispatch(fetchRecommendations(favsArray, locationObj, cuisineArray))
+    dispatch(fetchRecommendations(favsArray, locationObj, cuisineArray)),
+  handleAddFav: (cardData, user) => 
+    dispatch(postAddFavorite(cardData, user)),
+  handleDeleteFav: (cardData, user) => 
+    dispatch(postDeleteFavorite(cardData, user))
 });
 
 export default connect (mapStateToProps, mapDipatchToProps)(Recommendations)
