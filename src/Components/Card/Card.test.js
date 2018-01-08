9cb5a4e4ad6951054203a849ccdc8276aa220072
Julie Dursema <jmdursema@gmail.com>
2018-01-08 07:36:13 -0700

    test card more

--- src/Components/Card/Card.test.js
+++ src/Components/Card/Card.test.js
@@ -27,4 +27,94 @@ describe('card', () => {
 
      expect(card).toMatchSnapshot()
   })
+
+  it('should call handleAddFav on click if there is a user and the card is not in favorites', () => {
+    const mockFunc = jest.fn()
+    const mockFunc2 = jest.fn()
+
+    const cardObj = {name: 'Scales', data:{
+      hours: '9-5',
+      reservations: 'no'}
+    }
+
+    const card = shallow(<Card
+      info= {cardObj}
+      handleAddFav = {mockFunc}
+      handleDeleteFav = {mockFunc2}
+      user = {{name: 'Julie', uid: '3jriero384u2'}}
+      history = {[]}
+      favorited = {false}
+      favorites={[{name: 'Duck Fat'}]}
+     />)
+
+     expect(mockFunc.mock.calls.length).toEqual(0)
+
+    card.find('button').simulate('click')
+
+    expect(mockFunc.mock.calls.length).toEqual(1)
+
+  })
+
+  it('should call handleDeleteFav on click if there is auser and the card is in favorites', () => {
+    const mockFunc = jest.fn()
+    const mockFunc2 = jest.fn()
+
+    const cardObj = {name: 'Scales', data:{
+      hours: '9-5',
+      reservations: 'no'}
+    }
+
+    const card = shallow(<Card
+      info= {cardObj}
+      handleAddFav = {mockFunc}
+      handleDeleteFav = {mockFunc2}
+      user = {{name: 'Julie', uid: '3jriero384u2'}}
+      history = {[]}
+      favorited = {true}
+      favorites={[cardObj]}
+     />)
+
+     expect(mockFunc2.mock.calls.length).toEqual(0)
+
+    card.find('button').simulate('click')
+
+    expect(mockFunc2.mock.calls.length).toEqual(1)
+
+  })
+
+
+  it('shouldnt call handleAddFav or handleDeleteFav on click if there is no user', () => {
+    const mockFunc = jest.fn()
+    const mockFunc2 = jest.fn()
+
+    const cardObj = {name: 'Scales', data:{
+      hours: '9-5',
+      reservations: 'no'}
+    }
+
+    const card = shallow(<Card
+      info= {cardObj}
+      handleAddFav = {mockFunc}
+      handleDeleteFav = {mockFunc}
+      user = {{}}
+      history = {[]}
+      favorited = {false}
+      favorites={[{name: 'Duck Fat'}]}
+     />)
+
+     expect(mockFunc.mock.calls.length).toEqual(0)
+
+     expect(mockFunc2.mock.calls.length).toEqual(0)
+
+    card.find('button').simulate('click')
+
+    expect(mockFunc.mock.calls.length).toEqual(0)
+
+    expect(mockFunc2.mock.calls.length).toEqual(0)
+
+  })
+
+
+
+
 })
